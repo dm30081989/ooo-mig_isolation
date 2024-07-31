68647246b2fafbfe7dfe1ae98ec0bc12027f96bf
Darya <dtcheburkova@yandex.ru>
2024-07-31 16:35:09 +0300

    delete clicker

--- clicker.py
+++
@@ -1,8 +0,0 @@
-# import pyautogui
-# import time
-
-# time.sleep(10)
-
-# currentMouseX, currentMouseY = pyautogui.position() # Получаем XY координаты курсора.
-# print(currentMouseX, currentMouseY)
-# pyautogui.move(532, 801)
